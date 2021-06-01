d40e6663d7692ad4e117f7f8d243fb3484dbd7e4
adiatma85 <adiatma85@gmail.com>
2021-06-01 12:11:55 +0700

    Renew Perpustakaan.sql

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -1,93 +1,142 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 23, 2016 at 09:06 PM
--- Server version: 5.5.46-0ubuntu0.14.04.2
--- PHP Version: 5.5.9-1ubuntu4.16
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Jun 2021 pada 07.11
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `perpustakaan2`
+-- Database: `pbd`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `ID_PESANAN` int(11) NOT NULL,
+  `ID_KARYAWAN` int(11) DEFAULT NULL,
+  `ID_PELANGGAN` int(11) DEFAULT NULL,
+  `ID_BUKU` int(11) DEFAULT NULL,
+  `TGL_PINJAM` date DEFAULT NULL,
+  `TGL_KEMBALI` date DEFAULT NULL,
+  `TGL_EXPIRED` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `PERPANJANGAN` decimal(1,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pesanan`
+--
+
+INSERT INTO `pesanan` (`ID_PESANAN`, `ID_KARYAWAN`, `ID_PELANGGAN`, `ID_BUKU`, `TGL_PINJAM`, `TGL_KEMBALI`, `TGL_EXPIRED`, `PERPANJANGAN`) VALUES
+(1, 2, 1, 1, '2021-06-01', '0000-00-00', '2021-06-07 17:00:00', '0');
+
+--
+-- Trigger `pesanan`
+--
 DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `detail_penerbit`(IN `kode_penerbit_nya` CHAR(4))
-BEGIN
-SELECT  * FROM  tbl_penerbit WHERE kode_penerbit=kode_penerbit_nya;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `hapus_penerbit`(IN `kode_penerbit_nya` CHAR(4))
-BEGIN
-DELETE FROm  tbl_penerbit WHERE kode_penerbit=kode_penerbit_nya;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `tambah_penerbit`(IN `kode_penerbit_nya` VARCHAR(4), IN `nama_penerbit_nya` VARCHAR(150), IN `kota_penerbit_nya` VARCHAR(50), IN `email_penerbit_nya` VARCHAR(150), IN `web_penerbit_nya` VARCHAR(200), IN `telp_penerbit_nya` VARCHAR(20))
-BEGIN
-INSERT INTO tbl_penerbit (kode_penerbit,nama_penerbit,kota_penerbit,email_penerbit,website_penerbit,telepon_penerbit)
-VALUES ( kode_penerbit_nya, nama_penerbit_nya, kota_penerbit_nya,  email_penerbit_nya, web_penerbit_nya, telp_penerbit_nya);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `tampil_penerbit`()
-BEGIN
-SELECT *  FROM  tbl_penerbit;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ubah_penerbit`(IN kode_penerbit_nya VARCHAR(4), IN nama_penerbit_nya VARCHAR(150), IN kota_penerbit_nya VARCHAR(50), IN email_penerbit_nya VARCHAR(150), IN web_penerbit_nya VARCHAR(200), IN telp_penerbit_nya VARCHAR(20))
-BEGIN
-UPDATE  tbl_penerbit SET 
-nama_penerbit=nama_penerbit_nya,
-kota_penerbit=kota_penerbit_nya,
-email_penerbit=email_penerbit_nya,
-website_penerbit=web_penerbit_nya,
-telepon_penerbit=telp_penerbit_nya
-
-WHERE kode_penerbit=kode_penerbit_nya;
-END$$
-
+CREATE TRIGGER `AFT_INS_PEMINJAMAN` AFTER INSERT ON `pesanan` FOR EACH ROW BEGIN
+	DECLARE DIPINJAM INT;
+    SET DIPINJAM = CekPeminjamanBuku(NEW.ID_BUKU);
+	IF DIPINJAM = 1 THEN
+		BEGIN
+            UPDATE books
+	        SET books.IS_BORROWED=1
+        	WHERE books.ID_BUKU=NEW.ID_BUKU;
+		END;
+    ELSE
+    	BEGIN
+        	DELETE FROM `pesanan` WHERE `pesanan`.`ID_PESANAN` = NEW.ID_PESANAN;
+        END;
+	END IF;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `AFT_UPD_PEMINJAMAN_TANGGAL_ISI` AFTER UPDATE ON `pesanan` FOR EACH ROW BEGIN
+	IF NEW.TGL_KEMBALI != NULL THEN
+    BEGIN
+    	UPDATE books
+            SET books.IS_BORROWED=0
+            WHERE books.ID_BUKU=OLD.ID_BUKU;
+    END;
+    END IF;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `AFT_UPD_PEMINJAMAN_TANGGAL_TERISI` AFTER UPDATE ON `pesanan` FOR EACH ROW BEGIN
+	IF NEW.TGL_KEMBALI != NULL THEN
+    BEGIN
+    	DECLARE SELISIHTANGGAL INT;
+        SET SELISIHTANGGAL = DATEDIFF(OLD.TGL_EXPIRED, NEW.TGL_KEMBALI);
+        IF SELISIHTANGGAL < 0 THEN
+        	BEGIN
+            	INSERT INTO `denda` 
+                (NOMINAL, Pesanan_ID) 
+                VALUES (PenghitunganDenda(SELISIHTANGGAL), OLD.ID_PESANAN);
+            END;
+        END IF;
+    	UPDATE books
+            SET books.IS_BORROWED=0
+            WHERE books.ID_BUKU=OLD.ID_BUKU;
+    END;
+    END IF;
+END
+$$
 DELIMITER ;
 
--- --------------------------------------------------------
-
-
 --
--- Table structure for table `tbl_penerbit`
+-- Indexes for dumped tables
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_penerbit` (
-  `kode_penerbit` char(4) NOT NULL,
-  `nama_penerbit` varchar(150) NOT NULL,
-  `kota_penerbit` varchar(100) NOT NULL,
-  `email_penerbit` varchar(200) NOT NULL,
-  `website_penerbit` varchar(200) NOT NULL,
-  `telepon_penerbit` varchar(20) NOT NULL,
-  PRIMARY KEY (`kode_penerbit`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`ID_PESANAN`),
+  ADD UNIQUE KEY `PESANAN_PK` (`ID_PESANAN`),
+  ADD KEY `KARYAWAN_TO_PESANAN_FK` (`ID_KARYAWAN`),
+  ADD KEY `PELANGGAN_TO_PESANAN_FK` (`ID_PELANGGAN`),
+  ADD KEY `BUKU_TO_PESANAN_FK` (`ID_BUKU`);
 
 --
--- Dumping data for table `tbl_penerbit`
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
-INSERT INTO `tbl_penerbit` (`kode_penerbit`, `nama_penerbit`, `kota_penerbit`, `email_penerbit`, `website_penerbit`, `telepon_penerbit`) VALUES
-('P001', 'Andi Publisher', 'Yogyakarta', 'info@andipublisher.com', 'http://andipublisher.com', '034-879878978'),
-('P002', 'Gramedia', 'Jakarta', 'pub@gramedia.com', 'http://gramedia.com', '021'),
-('P003', 'Jasakom', 'Jakarta', 'penerbitan@jasakom.com', 'http://jasakom.com', '0217857656'),
-('P004', 'Media Kita', 'Surabaya', 'publisher@mediakita.com', 'http://mediakita.com', '023443543543'),
-('P006', 'Informatika', 'Bandung', 'publisher@informatika.com', 'http://informatika.com', '022-0987987987'),
-('P007', 'Erlanga', 'Surabaya', '0979878', 'http://erlanga.com', 'info@erlangga.com'),
-('P008', 'BPFE UGM', 'Yogyakarta', 'bpfe@ugm.ac.id', 'bpfe.ugm.ac.id', '097798787987'),
-('P009', 'Balai Buku Nasional', 'Jakarta', 'info@balaibuku.go.id', 'http://balaibuku.go.id', '0219879878');
+--
+-- AUTO_INCREMENT untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `ID_PESANAN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
--- --------------------------------------------------------
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`ID_KARYAWAN`) REFERENCES `karyawan` (`ID_KARYAWAN`),
+  ADD CONSTRAINT `pesanan_ibfk_2` FOREIGN KEY (`ID_PELANGGAN`) REFERENCES `pelanggan` (`ID_PELANGGAN`),
+  ADD CONSTRAINT `pesanan_ibfk_4` FOREIGN KEY (`ID_BUKU`) REFERENCES `books` (`ID_BUKU`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
